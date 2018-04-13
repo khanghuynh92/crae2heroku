af546beb1cefa85ce823f6eb30efb48830d4a486
Administrator <khanghuynh92@gmail.com>
2018-04-13 14:31:08 +0700

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  state = {cities: []}
-
-  async componentDidMount() {
-    const response = await fetch('/cities')
-    const cities   = await response.json()
-
-    this.setState({cities: cities})
+  constructor (props) {
+    super(props)
+    this.state = {
+      cities: []
+    }
   }
+  async componentDidMount () {
+    const response = await fetch('/cities')
+    const cities = await response.json()
 
-  render() {
+    this.setState({ cities })
+  }
+  render () {
     return (
       <div>
-        <h2>{process.env.REACT_APP_MY_VARIABLE}</h2>
         <ul>
-          {this.state.cities.map( city => {
+          {this.state.cities.map(city => {
             return <li key={city.name}> <b>{city.name}</b>: {city.population}</li>
           })}
         </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
